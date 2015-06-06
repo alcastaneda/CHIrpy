c1b5267d5a1b380b72a1e475187e9d828a952f64
Cynaria <Elizabeth.J.Cruz@gmail.com>
2015-06-06 14:35:04 -0500

    Added API subscriber route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  scope :api do
+    scope :subscribers do
+      post '/' => 'subscribers#create'
+    end
+  end
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
